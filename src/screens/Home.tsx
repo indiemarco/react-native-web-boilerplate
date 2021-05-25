6121244ab04bc23bd0252c43e7e312f0c0573171
indiemarco <indiemarco@protonmail.com>
2021-05-25 19:32:16 +0800

    first commit

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,7 +2,7 @@ import { View, Text } from 'react-native';
 export function Home() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+      <Text>hello Home Screen</Text>
     </View>
   );
 }

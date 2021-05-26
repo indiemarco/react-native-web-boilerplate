92e79b7b6f769be858c3d981bbc5c6a4b7dd7144
indiemarco <indiemarco@protonmail.com>
2021-05-26 12:22:59 +0800

    tidy up

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { App } from './src/App';
 
 const isWeb = Platform.OS === 'web';
 
-AppRegistry.registerComponent(isWeb ? 'main' : 'monorepo', () => App);
+AppRegistry.registerComponent(isWeb ? 'main' : 'RNW', () => App);
 if (isWeb) {
   const rootTag =
     document.getElementById('root') ?? document.getElementById('main');
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useColorScheme } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Home } from 'screens/Home';
@@ -6,7 +5,6 @@ import { Home } from 'screens/Home';
 const Stack = createStackNavigator();
 
 export function App() {
-  const scheme = useColorScheme();
   return (
     <NavigationContainer>
       <Stack.Navigator>
